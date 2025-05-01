5865d987434d63f70fb263a3550448595811ceb8
Simge Helvaci <simgehelvaci@gmail.com>
2025-05-01 23:40:37 +0300

    Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,21 +4,25 @@ import './Navbar.css'
 const Navbar = () => {
   return (
     <nav className="navbar">
+
       <div className="nav-left">
         <Link to="/" className="logo">traveller.</Link>
       </div>
-
+      
+    
       <div className="nav-center">
         <Link to="/discover" className="nav-link">Discover</Link>
         <Link to="/feed" className="nav-link">Feed</Link>
         <Link to="/friends" className="nav-link">Friends</Link>
+    
       </div>
-
+    
       <div className="nav-right">
+        
         <Link to="/login" className="btn btn-blue">Sign In</Link>
         <Link to="/register" className="btn">Register</Link>
-        </div>
-
+      </div>
+      
     </nav>
   )
 }
