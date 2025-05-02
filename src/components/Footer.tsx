d0d0f9c34bdb9203591e6a227af91409bc641e57
Simge Helvaci <simgehelvaci@gmail.com>
2025-05-02 22:24:50 +0300

    footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import './Footer.css'
+
+export default function Footer() {
+  return (
+    <div className="footer-section">
+      Ben bir footer’ım. Ayakta duruyorum.
+    </div>
+  )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import SearchBar from '../components/SearchBar'
 import MidBar from '../components/MidBar'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
     <div className="pt-32">
       <SearchBar />
       <MidBar />
+      <Footer />
     </div>
   )
 }
